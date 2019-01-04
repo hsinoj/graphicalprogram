ca7bae27ffba6c5a0a5ef8d803039b29389c9180
jonish prajapai <wraithk94@gmail.com>
2019-01-04 12:43:55 +0545

    form are created

---
+++ GeometryProgram/GeometryProgram/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GeometryProgram
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            eometry_Design ed = new eometry_Design(); //creating instance of another form
+            ed.Show(); //opening the specific form
+            Visible = false; //closing the current form
+        }
+    }
+}
---
+++ GeometryProgram/GeometryProgram/eometry_Design.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GeometryProgram
+{
+    public partial class eometry_Design : Form
+    {
+        public eometry_Design()
+        {
+            InitializeComponent();
+        }
+    }
+}

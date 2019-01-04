b56a9e2279b5aba22bd4984c54e30b34959b75c1
jonish prajapai <wraithk94@gmail.com>
2019-01-04 23:59:55 +0545

    save and load

--- GeometryProgram/GeometryProgram/eometry_Design.cs
+++ GeometryProgram/GeometryProgram/eometry_Design.cs
@@ -44,30 +44,49 @@ namespace GeometryProgram
             run.Visible = true;
         }
 
-        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        private async void saveToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            SaveFileDialog sfd = new SaveFileDialog();//creating a instance of a dialog box
+            sfd.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*"; //creting text form
+            sfd.FilterIndex = 2;
+            sfd.RestoreDirectory = true;
 
+            if (sfd.ShowDialog() == DialogResult.OK)//if choose data is fine then enter todialog box
+            {
+                try
+                {
+                    
+                        using (StreamWriter write = new StreamWriter(sfd.FileName)) //dispose is done automatically
+                        {
+                            await write.WriteLineAsync(edit.Text);//loading text file in textbox
+                        }
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
         }
 
         private void loadToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            Stream myStream = null;
-            OpenFileDialog openFileDialog1 = new OpenFileDialog();
-
+            Stream myStream = null;//instantiating null value for stra
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();//creating a instance of a dialog box
             openFileDialog1.InitialDirectory = "c:\\";
-            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*"; //creting text form
             openFileDialog1.FilterIndex = 2;
             openFileDialog1.RestoreDirectory = true;
 
-            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)//if choose data is fine then enter todialog box
             {
                 try
                 {
-                    if ((myStream = openFileDialog1.OpenFile()) != null)
+                    if ((myStream = openFileDialog1.OpenFile()) != null) //if the choosen file is not empty then the file is read
                     {
-                        using (myStream)
+                        using (myStream) //dispose is done automatically
                         {
-                            edit.Text = File.ReadAllText(openFileDialog1.FileName);
+                            edit.Text = File.ReadAllText(openFileDialog1.FileName);//loading text file in textbox
                         }
                     }
                 }
